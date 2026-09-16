dd74d8363a390619fe3cff25ca6c869ade7e2255
Serotonina333 <seleelol33@gmail.com>
2026-09-16 08:30:37 +0200

    Add files via upload

---
+++ 111111/09.09.2026/lekcja 1/ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace kalkulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                Console.WriteLine("Podaj 1 liczbę:");
+
+                double liczba1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Podaj działanie");
+                string znak = Console.ReadLine();
+
+                Console.WriteLine("Podaj 2 liczbę:");
+
+                double liczba2 = Convert.ToDouble(Console.ReadLine());
+
+                double wynik = 0;
+
+
+                switch (znak)
+                {
+                    case "+":
+                        wynik = liczba1 + liczba2;
+                        break;
+                    case "-":
+                        wynik = liczba1 - liczba2;
+                        break;
+                    case "*":
+                        wynik = liczba1 * liczba2;
+                        break;
+                    case "/":
+                        if (liczba2 == 0)
+                            throw new Exception("dzielenie przez 0");
+                        wynik = liczba1 / liczba2;
+                        break;
+                        
+                    default:
+
+                        break;
+                }
+                Console.WriteLine("wynik: " + wynik);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+                
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 111111/09.09.2026/lekcja 1/lekcja 1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace lekcja_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string imie;
+
+            Console.WriteLine("Podaj imię:");
+            imie = Console.ReadLine();
+
+            string nazwisko;
+
+            Console.WriteLine("Podaj nazwisko:");
+            nazwisko = Console.ReadLine();
+
+            string wiek;
+
+            Console.WriteLine("Podaj wiek:");
+            wiek = Console.ReadLine();
+
+            string wyksztalcenie;
+
+            Console.WriteLine("Podaj wyksztalcenie:");
+            wyksztalcenie = Console.ReadLine();
+
+            Console.WriteLine("Witaj: " + imie + nazwisko + "\n" + "twój wiek to:" + wiek + "\n" + "twoje wykształcenie to:" + wyksztalcenie);
+             
+
+            Console.ReadKey();
+        }
+    }
+}
